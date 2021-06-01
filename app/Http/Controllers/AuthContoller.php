b369a8dd40852e7a102a06cc552fb19c440e5386
yoengvisal <yoengvisal@gmail.com>
2021-06-01 14:33:11 +0700

    my first commit

---
+++ app/Http/Controllers/AuthContoller.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthContoller extends Controller
+{
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response,201);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthContoller;
+// use GuzzleHttp\Middleware;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::resource('products', ProductController::class);
+// publics routes
+Route::post('/registers',[AuthContoller::class, 'register']);
+Route::get('/products',[ProductController::class, 'index']);
+Route::get('/products/{id}',[ProductController::class, 'show'] );
+Route::get('/products/search/{name}',[ProductController::class, 'search'] );
+
+
+Route::group(['middleware'=>['auth:sanctum']], function() {
+    Route::post('/products',[ProductController::class,'store']);
+    Route::put('/products/{id}',[ProductController::class,'update']);
+    Route::delete('/products/{id}',[ProductController::class,'destroy']);
+});
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
